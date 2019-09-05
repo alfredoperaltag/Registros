ba1b28ac63e2a4c7bad7cc48b94eb16adc1c161b
alfredoperaltag <alfredo08peralta@gmail.com>
2019-09-05 17:13:33 -0500

    algoritmo 2.0

--- src/main/java/com/example/registro/implementaciones/RegistroServiceImpl.java
+++ src/main/java/com/example/registro/implementaciones/RegistroServiceImpl.java
@@ -20,6 +20,20 @@ private Logger log = LoggerFactory.getLogger(this.getClass());
 	@Qualifier ("RegistroRepository")
 	private RegistroRepository registroRepository;
 	
+	private String digitos(int variable, String primero,String segundo,String tercero) {
+		String resultado="";
+		if(variable<10) {
+			return resultado=primero+Integer.toString(variable);
+		} else if(variable<100) {
+			return resultado=segundo+Integer.toString(variable);
+		} else if(variable<1000) {
+			return resultado=tercero+Integer.toString(variable);
+		} else if(variable<10000) {
+			return resultado=Integer.toString(variable);
+		}
+		return resultado;
+	}
+	
 	@Override
 	public boolean agregarRegistro() {
 		String ka="";
@@ -28,30 +42,13 @@ private Logger log = LoggerFactory.getLogger(this.getClass());
 		String cuenta="";
 		try{
 			for(int i=0001; i<10000; i++) {
-				if(i<10) {
-					ii="000"+Integer.toString(i);
-				} else if(i<100) {
-					ii="00"+Integer.toString(i);
-				} else if(i<1000) {
-					ii="0"+Integer.toString(i);
-				} else if(i<10000) {
-					ii=Integer.toString(i);
-				}
+				ii=digitos(i,"000","00","0");
 				for(int j=00; j<100; j++) {
-					if(j<10) {
-						jo="0"+Integer.toString(j);
-					}else if(j<100) {
-						jo=Integer.toString(j);
-					}
+					jo=digitos(j,"0","","");
 					if(j!=0) {
 						for(int k=000; k<1000; k++) {
-							if(k<10) {
-								ka="00"+Integer.toString(k);
-							}else if(k<100) {
-								ka="0"+Integer.toString(k);
-							}else if(k<1000) {
-								ka=Integer.toString(k);
-							}
+							ka=digitos(k,"00","0","");
+							log.info(ii+"-"+jo+"-"+ka);
 							cuenta=ii+"-"+jo+"-"+ka;
 							Registro registro = new Registro(1,1,1,Date.valueOf("2019-07-10"),1,cuenta,"nombre",1,(char) 1,"mxn",1,1);
 							registroRepository.save(registro);
